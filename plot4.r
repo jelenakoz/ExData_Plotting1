47f2c8668f9ce5d9f9568e4e984ded42a09852a4
jelenakoz <kozoderovic@yahoo.com>
2015-04-10 09:17:20 +0200

    Update plot4.r

--- plot4.r
+++ plot4.r
@@ -6,7 +6,7 @@ imena<-colnames(tab5rows)
 dataSet <- read.table("household_power_consumption.txt",sep=";",as.is=TRUE,col.names=imena, ,skip=66637, nrows = 2880)
 
 
-#cetvrti
+#Plot
 par(mfrow = c(2, 2))
 plot(as.numeric(dataSet[,3]),type="l",xlab="",xaxt="n",ylab="Global Active Power (kilowatts)")
 axis(1,at=c(1,1440,2880),labels=c("Thu","Fri","Sat"))
