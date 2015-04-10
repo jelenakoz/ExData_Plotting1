b1633c910e1469f3901ef9571e75ca8455729b61
jelenakoz <kozoderovic@yahoo.com>
2015-04-10 09:43:52 +0200

    Update plot4.r

--- plot4.r
+++ plot4.r
@@ -5,8 +5,8 @@ tab5rows <- read.table("household_power_consumption.txt", sep=";",header = TRUE,
 imena<-colnames(tab5rows)
 dataSet <- read.table("household_power_consumption.txt",sep=";",as.is=TRUE,col.names=imena, ,skip=66637, nrows = 2880)
 
-
-#Plot
+windows(width=480, height=480)
+#cetvrti
 par(mfrow = c(2, 2))
 plot(as.numeric(dataSet[,3]),type="l",xlab="",xaxt="n",ylab="Global Active Power (kilowatts)")
 axis(1,at=c(1,1440,2880),labels=c("Thu","Fri","Sat"))
@@ -20,7 +20,7 @@ plot(as.numeric(dataSet[,8]),type = "l",ylim=range(as.numeric(dataSet[,7])),ylab
 par(new=TRUE)
 plot(as.numeric(dataSet[,9]),type = "l",ylab="",ylim=range(as.numeric(dataSet[,7])),yaxt="n",xlab="",xaxt="n",col="blue")
 axis(1,at=c(1,1440,2880),labels=c("Thu","Fri","Sat"))
-legend("topright",legend=colnames(dataSet)[7:9],bty="n",lty=1,col=c("black","blue","red"))
+legend("topright",legend=colnames(dataSet)[7:9],bty="n",lty=1,col=c("black","blue","red"),cex=0.8)
 
 plot(as.numeric(dataSet[,4]),type="l",xaxt="n",ylab="Global_reactive_power",xlab="datetime")
 axis(1,at=c(1,1440,2880),labels=c("Thu","Fri","Sat"))
